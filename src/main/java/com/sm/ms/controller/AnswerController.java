3833a3ea11a911ef171cac1863915f34fa4adf37
Tri Pham <pmtri@gmail.com>
2019-11-29 11:09:27 +0700

    update lai cac package

--- src/main/java/com/sm/ms/controller/AnswerController.java
+++ src/main/java/com/sm/ms/controller/AnswerController.java
@@ -1,13 +1,13 @@
 package tri.lo.controller;
 
+import com.sm.ms.model.question.Answer;
+import com.sm.ms.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import tri.lo.model.Answer;
-import tri.lo.service.AnswerService;
 
 import java.util.List;
 
